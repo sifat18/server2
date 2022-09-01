cce97eb5f76511a122045f956b3849afa4fb8dee
Sifat <ibnsifat@gmail.com>
2022-09-02 03:12:24 +0600

    'route'

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:9000",
+    baseURL: "https://cryptic-forest-09082.herokuapp.com",
 });
 
 export default axiosInstance;
